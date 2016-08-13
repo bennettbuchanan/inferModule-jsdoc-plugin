597e45ff167ee609bb622976be6bf6b4153a1b20
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-08-13 13:52:27 -0700

    Add new plugin and test file.

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
-///
-/// @module conversion
-/// @desc This module contains utilities used for converting Relax NG files to
-/// the format required by salve.
-/// @author Louis-Dominique Dubeau
-/// @license MPL 2.0
-/// @copyright 2013-2015 Mangalam Research Center for Buddhist Languages
-///
+/**
+ * @module conversion
+ * @desc This module contains utilities used for converting Relax NG files to
+ * the format required by salve.
+ * @author Louis-Dominique Dubeau
+ * @license MPL 2.0
+ * @copyright 2013-2015 Mangalam Research Center for Buddhist Languages
+ */
 define(/** @lends module:conversion */
   function conversion(require, exports, _module) {
     "use strict";
