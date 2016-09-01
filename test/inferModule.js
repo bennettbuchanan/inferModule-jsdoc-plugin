3bc8c6b51b0e3c87f84e13ed9fc3b01b6728c211
Bennett Buchanan <buchanan.3rd@gmail.com>
2016-09-01 14:30:56 -0700

    Add tests, add exclude option in conf.json.

--- test/inferModule.js
+++ test/inferModule.js
@@ -1,20 +1,46 @@
-var expect    = require("chai").expect;
+var assert = require("chai").assert;
+var expect = require("chai").expect;
 var inferModule = require("../node_modules/jsdoc/plugins/inferModule.js");
+var visitNode = inferModule.astNodeVisitor.visitNode;
+
 var node = { comments: [ { raw: '' } ] };
+var op_conf = {
+    "inferModule":{
+        "exclude": "lib/foo/*.",
+        "schema": [
+            { "from": "^lib\\/foo\\/(.*)\\.js$", "to": "bar/$1" },
+            { "from": "^lib\\/fin\\/(.*)\\.js$", "to": "baz/$1" },
+            { "from": "^lib\\/(.*\\.js$)", "to": "$1"}
+        ]
+    }
+}
 
 describe("Infer Module", function() {
     it("Converts the path appropriately for module naming.", function() {
-        expect(inferModule.astNodeVisitor.visitNode(
-            node, {}, {}, 'lib/foo/a.js', true)).to.equal('bar/a');
+        console.log(visitNode(node, {}, {}, 'lib/foo/a.js', op_conf));
+        expect(visitNode(node, {}, {}, 'lib/foo/a.js', op_conf)).to.equal('bar/a');
     });
 
     it("Regiters different mapping.", function() {
-        expect(inferModule.astNodeVisitor.visitNode(
-            node, {}, {}, 'lib/fin/a.js', true)).to.equal('baz/a');
+        expect(visitNode(node, {}, {}, 'lib/fin/a.js', op_conf)).to.equal('baz/a');
     });
 
     it("Handles a file with no path.", function() {
-        expect(inferModule.astNodeVisitor.visitNode(
-            node, {}, {}, 'lib/a.js', true)).to.equal('a');
+        expect(visitNode(node, {}, {}, 'lib/a.js', op_conf)).to.equal('a');
+    });
+
+    it("Does not alter naming of differing extensions.", function() {
+        expect(visitNode(node, {}, {}, 'lib/a.py', op_conf)).to.equal('lib/a');
+    });
+
+    // it("If no conf.json is specified, throw an error.", function() {
+    //     expect(visitNode.bind(node, {}, {}, 'lib/a.py', op_conf)).to.throw(
+    //         "No conf.json specifieds.");
+    // });
+
+    it("If module tag is already present, use it.", function () {
+        node = { comments: [ { raw: '**\n * @module this/that' } ] };
+
+        expect(visitNode(node, {}, {}, 'lib/a.js', op_conf)).to.equal(undefined);
     });
 });
